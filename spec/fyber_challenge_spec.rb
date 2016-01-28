04445673364d1050f8a40ed0bc3663d27a47f0a1
Artyom Pertsovsky <artyom@pertsovsky.com>
2016-01-28 21:15:14 +0200

    Change formatting to be more consistent

--- spec/fyber_challenge_spec.rb
+++ spec/fyber_challenge_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe 'Fyber challenge', :type => :feature, vcr: without_timestamp_and_hashkey do
+describe 'Fyber challenge', type: :feature, vcr: without_timestamp_and_hashkey do
   before(:each) do
     visit '/'
   end
 
   context 'given uid, pub0, page are VALID' do
     it 'should render offers' do
-      fill_in 'uid',  :with => 'Player1'
-      fill_in 'pub0', :with => 'campaign2'
-      fill_in 'page', :with => '1'
+      fill_in 'uid',  with: 'player1'
+      fill_in 'pub0', with: 'campaign2'
+      fill_in 'page', with: '1'
 
       click_button 'Submit'
       expect(page).to have_css('table tr td')
@@ -18,9 +18,9 @@ describe 'Fyber challenge', :type => :feature, vcr: without_timestamp_and_hashke
 
   context 'given uid, pub0, page are INVALID' do
     it "should render 'No offers found'" do
-      fill_in 'uid',  :with => 'invalid_uid'
-      fill_in 'pub0', :with => 'invalid_pub0'
-      fill_in 'page', :with => '1000'
+      fill_in 'uid',  with: 'invalid_uid'
+      fill_in 'pub0', with: 'invalid_pub0'
+      fill_in 'page', with: '1000'
 
       click_button 'Submit'
       expect(page).to have_content('No offers found')
